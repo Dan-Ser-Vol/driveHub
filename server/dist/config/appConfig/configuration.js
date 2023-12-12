9037db5a3e53c929f036f6913451346c66382f4a
Sergey <62606319+Dan-Ser-Vol@users.noreply.github.com>
2023-12-12 13:05:33 +0200

    before checkout to master

---
+++ server/dist/config/appConfig/configuration.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const process = require("process");
+exports.default = () => ({
+    front_url: process.env.FRONT_URL,
+});
+//# sourceMappingURL=configuration.js.map
